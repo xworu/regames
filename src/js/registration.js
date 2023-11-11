e404bf9f022d110e470c482e987e739299d2290a
Liza <liza.yemelyanova@list.ru>
2023-11-11 14:37:45 +0300

    create transition between registration and login pages

---
+++ src/js/registration.js
@@ -0,0 +1,6 @@
+const btnLogin = document.getElementById('login_btn');
+
+btnLogin.addEventListener('click', () => {
+    window.location.pathname = '/src/html/login.html';
+    // location.href += '/login.html';
+})

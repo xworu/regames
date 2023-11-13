b041bac98afcf287c918dc3f1742a2f4cf002db2
Liza <liza.yemelyanova@list.ru>
2023-11-14 00:49:38 +0300

    create games page

---
+++ src/js/games.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.avatar');
+
+btn.addEventListener('click', () => {
+    window.location.pathname = '/src/html/registration.html';
+})
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('go');
+
+btn.addEventListener('click', () => {
+    window.location.pathname = '/src/html/games.html';
+})

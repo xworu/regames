d4b0d8b903ace35b68b1d82af26302406126251c
Liza <liza.yemelyanova@list.ru>
2023-11-28 13:23:09 +0300

    add tetris

--- src/js/games.js
+++ src/js/games.js
@@ -1,5 +1,10 @@
-const btn = document.querySelector('.avatar');
+const btn = document.querySelector('.avatar'),
+      tetris = document.querySelector('#tetris');
 
 btn.addEventListener('click', () => {
     window.location.pathname = '/src/html/registration.html';
+})
+
+tetris.addEventListener('click', () => {
+    window.location.pathname = '/tetris/index.html';
 })
